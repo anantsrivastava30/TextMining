3f5d8b3260d985065d70cfbfb88bf25d452ce47f
anant <anantsrivastava30@gmail.com>
2017-12-24 14:36:36 -0700

    series normalized abd sorted

--- dates.py
+++ dates.py
@@ -53,40 +53,36 @@
 # 
 # *This function should return a Series of length 500 and dtype int.*
 
-# In[118]:
+# In[1]:
 
 import pandas as pd
 import re
 import numpy 
 
 
-doc = []
-with open('dates.txt') as file:
-    for line in file:
-        doc.append(line)
+# In[2]:
 
-df = pd.Series(doc)
-isinstance(df, pd.Series)
+def date_sorter():
 
+    doc = []
+    with open('dates.txt') as file:
+        for line in file:
+            doc.append(line)
 
-# In[119]:
-
-df = df.str.replace(r'(?<=[/,])\d\d(?=[ |.=\n)])', lambda x : '19' + x.group(0))
-# '-' can be used to indicate minute to minute relationship
-df = df.str.replace(r'(?<=[-]\d\d[-])\d\d(?=[ |.=\n)])', lambda x : '19' + x.group(0))
-isinstance(df, pd.Series)
-
-
-# In[120]:
-
-df[7]
-
-
-# In[121]:
-
-data = df.str.extract(r"""
+    df = pd.Series(doc)
+    #isinstance(df, pd.Series)
+        
+    # Your code here
+    isinstance(df, pd.Series)
+    
+    df = df.str.replace(r'(?<=[/,])\d\d(?=[ |.=\n)])', lambda x : '19' + x.group(0))
+    # '-' can be used to indicate minute to minute relationship
+    df = df.str.replace(r'(?<=[-]\d\d[-])\d\d(?=[ |.=\n)])', lambda x : '19' + x.group(0))
+    #isinstance(df, pd.Series)
+    
+    data = df.str.extract(r"""
                 (
-                \d{1,2} |  Jan(?:[au][au]ry)? | Feb(?:ruary)? | Mar(?:ch)? | Apr(?:il)? | May? | Jun(?:e)? | Jul(?:y)? |
+                (?<![.])\d{1,2} |  Jan(?:[au][au]ry)? | Feb(?:ruary)? | Mar(?:ch)? | Apr(?:il)? | May? | Jun(?:e)? | Jul(?:y)? |
                            Aug(?:ust)? | Sep(?:tember)? | Oct(?:ober)? | Nov(?:ember)? | Dec(?:em(?:e)?ber)? 
                 )??
                 [\W]*?
@@ -100,34 +96,52 @@ data = df.str.extract(r"""
                 )
                 """,re.X)
 
+    null_col = data.columns[data.isnull().any()]
+    #print(data[null_col].isnull().sum())
+    data.update(data[data[1].isnull()].fillna('1'))
+    data.update(data[data[0].isnull()].fillna('1'))
 
-# In[124]:
+    # print(data.head(200).tail(30))
+    #print(isinstance(df, pd.Series))
+    #print(isinstance(data, pd.DataFrame))
 
-null_col = data.columns[data.isnull().any()]
-print(data[null_col].isnull().sum())
-data.update(data[data[1].isnull()].fillna('1'))
-data.update(data[data[0].isnull()].fillna('1'))
+    x = data.as_matrix()
+    count = 0
+    for row in range(x.shape[0]):
+        count += 1 
+        if not x[row][1].isdigit() and x[row][0].isdigit():
+            x[row][0], x[row][1] = x[row][1], x[row][0]
+        
+    #print(count)
+    data.replace(r'Ja.*', '1', inplace=True, regex=True)
+    data.replace(r'Feb.*', '2', inplace=True, regex=True)
+    data.replace(r'Mar.*', '3', inplace=True, regex=True)
+    data.replace(r'Apr.*', '4', inplace=True, regex=True)
+    data.replace(r'May.*', '5', inplace=True, regex=True)
+    data.replace(r'Jun.*', '6', inplace=True, regex=True)
+    data.replace(r'Jul.*', '7', inplace=True, regex=True)
+    data.replace(r'Aug.*', '8', inplace=True, regex=True)
+    data.replace(r'Sep.*', '9', inplace=True, regex=True)
+    data.replace(r'Oct.*', '10', inplace=True, regex=True)
+    data.replace(r'Nov.*', '11', inplace=True, regex=True)
+    data.replace(r'Dec.*', '12', inplace=True, regex=True)
 
-print(isinstance(df, pd.Series))
-print(isinstance(data, pd.DataFrame))
-#def func(row):
-#    return row[2]
+    data[0] = data.apply(lambda x : int(x[0]), axis=1)
+    data[1] = data.apply(lambda x : int(x[1]), axis=1)
+    data[2] = data.apply(lambda x : int(x[2]), axis=1)
+    data.sort_index(inplace=True,sort_remaining=True,by=[2,0,1])
+    # data = data.apply(lambda x : x if x[1].isdigit() else x.reindex([2,0,1]), axis=1)
+    # print(data.index.values)
+    
+    return pd.Series(data.index.values)
 
-data = data.apply(lambda x : x if x[1].isdigit() else x.reindex([2,0,1]), axis=1)
 
-data.head(200).tail(30)
+# In[3]:
+
+date_sorter()
 
 
 # In[ ]:
 
 
-
-
-# In[123]:
-
-def date_sorter(df):
-    
-    # Your code here
-    
-    return # Your answer here
 
